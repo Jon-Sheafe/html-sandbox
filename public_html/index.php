cb062fbbfc3893785efcc26da7e334212a0bb593
Jon Sheafe <msckj@yahoo.com>
2016-10-11 11:53:17 -0600

    first html page

---
+++ public_html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>HTML Sandbox</title>
+	</head>
+	<body>
+		<h1>Hello, World! :D</h1>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: finef
+ * Date: 10/11/2016
+ * Time: 11:04 AM
+ */
+?>
+
+	</body>
+</html>
+
+
