c23e7d7de8a7a51001350d86170b00c9bf65e9e3
Jon Sheafe <msckj@yahoo.com>
2016-10-11 13:37:59 -0600

    first html page

---
+++ public_html/hi.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Hi! This is my Page</title>
+	</head>
+	<body>
+		<header><!-- this is the header -->
+			<h1>Hi! This is my page</h1>
+		</header>
+
+		<!--main-->
+		<main>
+			<!--begin section 1-->
+			<section>
+				<p>
+					How long can two people talk about nothing? Worf, It's better than music. It's jazz. Did you come here for something in particular or just general Riker-bashing? I've had twelve years to think about it. And if I had it to do over again, I would have grabbed the phaser and pointed it at you instead of them. Shields up! Rrrrred alert! My oath is between Captain Kargan and myself. Your only concern is with how you obey my orders. Or do you prefer the rank of prisoner to that of lieutenant? I'll be sure to note that in my log. The Federation's gone; the Borg is everywhere! You bet I'm agitated! I may be surrounded by insanity, but I am not insane. Mr. Worf, you sound like a man who's asking his friend if he can start dating his sister.
+				</p>
+					<img src="img/heart.jpg" alt="heart">
+			</section>
+			<section></section>
+			<section></section>
+		</main>
+		<h1>Hello, World! :D</h1>
+
+
+	</body>
+</html>
+
+
--- public_html/index.php
+++ public_html/index.php
@@ -6,14 +6,7 @@
 	</head>
 	<body>
 		<h1>Hello, World! :D</h1>
-<?php
-/**
- * Created by PhpStorm.
- * User: finef
- * Date: 10/11/2016
- * Time: 11:04 AM
- */
-?>
+
 
 	</body>
 </html>
